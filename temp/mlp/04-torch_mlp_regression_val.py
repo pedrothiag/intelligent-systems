13860255981789dad1bbbd13a378be1d6a44d7c3
pedrothiag <pedrothiag@gmail.com>
2026-04-21 19:21:16 -0300

    Adicionado o notebook sobre redes MLP

--- temp/mlp/04-torch_mlp_regression_val.py
+++ temp/mlp/04-torch_mlp_regression_val.py
@@ -16,7 +16,7 @@ y = data.target.reshape(-1, 1)
 
 # Divisão em conjunto de treinamento (70%), validação (15%) e teste (15%)
 X_train, X_temp, y_train, y_temp = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.3, random_state=42
 )
 X_val, X_test, y_val, y_test = train_test_split(
     X_temp, y_temp, test_size=0.5, random_state=42
